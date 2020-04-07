fbcf83a1d7965d3e3d72ec5baee86b083567867c
Tyler Kummer <tylerkummer@protonmail.com>
2020-04-07 16:42:55 -0500

    Get Requests

--- posts/postsRouter.js
+++ posts/postsRouter.js
@@ -10,8 +10,52 @@ router.get("/", (req, res) => {
       res.status(200).json(posts);
     })
     .catch((error) => {
+      // log error to database
+      console.log(error);
       res.status(500).json({
-        message: "Error retrieving the posts",
+        error: "The posts information could not be retrieved.",
       });
     });
 });
+
+router.get("/:id", (req, res) => {
+  Posts.findById(req.params.id)
+    .then((post) => {
+      if (post) {
+        res.status(200).json(post);
+      } else {
+        res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist." });
+      }
+    })
+    .catch((error) => {
+      // log error to database
+      console.log(error);
+      res.status(500).json({
+        error: "The post information could not be retrieved.",
+      });
+    });
+});
+
+router.get("/:id/comments", (req, res) => {
+  Posts.findPostComments(req.params.id)
+    .then((post) => {
+      if (post) {
+        res.status(200).json(post);
+      } else {
+        res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist." });
+      }
+    })
+    .catch((error) => {
+      // log error to database
+      console.log(error);
+      res.status(500).json({
+        error: "The comments information could not be retrieved.",
+      });
+    });
+});
+
+module.exports = router;
