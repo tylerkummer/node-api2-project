e2be0514c7a2f74495f9cc2c5f10ff5f85c9ed8a
Tyler Kummer <tylerkummer@protonmail.com>
2020-04-07 16:54:36 -0500

    Delete

--- posts/postsRouter.js
+++ posts/postsRouter.js
@@ -58,4 +58,21 @@ router.get("/:id/comments", (req, res) => {
     });
 });
 
+router.delete("/:id", (req, res) => {
+  Posts.remove(req.params.id)
+    .then((post) => {
+      if (post) {
+        res.status(200).json({ message: "Deleted" });
+      } else {
+        res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist." });
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({ error: "The post could not be removed" });
+    });
+});
+
 module.exports = router;
