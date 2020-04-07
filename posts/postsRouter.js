f802108274345c5e94bc2699b23f2e1e96f09803
Tyler Kummer <tylerkummer@protonmail.com>
2020-04-07 17:13:23 -0500

    Post and Put

--- posts/postsRouter.js
+++ posts/postsRouter.js
@@ -4,6 +4,46 @@ const Posts = require("../data/db");
 
 const router = express.Router();
 
+router.post("/", (req, res) => {
+  Posts.add(req.query)
+    .then((post) => {
+      if (post) {
+        res.status(201).json(post);
+      } else {
+        res.status(400).json({
+          errorMessage: "Please provide title and contents for the post.",
+        });
+      }
+    })
+    .catch((error) => {
+      // log error to database
+      console.log(error);
+      res.status(500).json({
+        error: "There was an error while saving the post to the database",
+      });
+    });
+});
+
+router.post("/:id/comments", (req, res) => {
+  Posts.add(req.query.id)
+    .then((post) => {
+      if (post) {
+        res.status(201).json(post);
+      } else {
+        res.status(404).json({
+          message: "The post with the specified ID does not exist.",
+        });
+      }
+    })
+    .catch((error) => {
+      // log error to database
+      console.log(error);
+      res.status(500).json({
+        error: "There was an error while saving the comment to the database",
+      });
+    });
+});
+
 router.get("/", (req, res) => {
   Posts.find(req.query)
     .then((posts) => {
@@ -72,6 +112,37 @@ router.delete("/:id", (req, res) => {
     .catch((error) => {
       console.log(error);
       res.status(500).json({ error: "The post could not be removed" });
+    });
+});
+
+router.put("/:id", (req, res) => {
+  Posts.update(req.params.id, req.body)
+    .then((post) => {
+      if (post) {
+        Posts.findById(req.params.id)
+          .then((post) => {
+            res.status(200).json(post);
+          })
+          .catch((error) => {
+            console.log(error);
+            res
+              .status(500)
+              .json({
+                errorMessage: "The post information could not be modified.",
+              });
+          });
+      } else {
+        res
+          .status(404)
+          .json({ message: "The post with the specified ID does not exist." });
+      }
+    })
+    .catch((error) => {
+      //log error to database
+      console.log(error);
+      res.status(500).json({
+        error: "The post information could not be modified.",
+      });
     });
 });
 
